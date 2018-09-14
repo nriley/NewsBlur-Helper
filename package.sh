a3740fc08fb376fa36b241ca4317e51499021114
Nicholas Riley <com-github@sabi.net>
2018-09-14 13:43:21 -0400

    Beginnings of packaging script.
    Need to replace the DSA key with one generated on 10.12 as it breaks.  For more:
    
    https://github.com/sparkle-project/Sparkle/issues/1180

---
+++ package.sh
@@ -0,0 +1,59 @@
+#!/bin/zsh -e
+
+set -x -v
+
+PACKAGEDIR="$PWD"
+ARCHIVEDIR="$PWD"/Archives
+EXPORTDIR=$(mktemp -d)
+RELEASEDIR="$PWD"/Releases
+PRODUCT=$(print *.xcodeproj(:r))
+
+# gather information
+VERSION=$(agvtool mvers -terse1)
+BUILD=$(agvtool vers -terse)
+DMG="$RELEASEDIR/$PRODUCT $VERSION.dmg"
+VOL="$PRODUCT $VERSION"
+ARCHIVE="$ARCHIVEDIR/$PRODUCT $VERSION ($BUILD).xcarchive"
+EXPORT="$EXPORTDIR/$VOL"
+
+# archive and export
+mkdir -p $ARCHIVEDIR
+rm -rf $ARCHIVE
+xcodebuild -scheme $PRODUCT -archivePath $ARCHIVE archive
+xcodebuild -archivePath $ARCHIVE -exportArchive -exportPath $EXPORT -exportOptionsPlist exportOptions.plist
+
+# ensure code signature and Developer ID are valid
+codesign --verify --verbose=4 "$EXPORT"/*.app
+# also capture the identity in order to sign the disk image
+IDENTITY=$(spctl -vv --assess "$EXPORT"/*.app 2>&1 | grep 'origin=' | sed -e 's/^origin=//')
+
+# remove export metadata we don't want in the disk image
+rm -f $EXPORT/*.plist $EXPORT/Packaging.log
+
+# create disk image
+mkdir -p $RELEASEDIR
+rm -f $DMG
+hdiutil create $DMG -megabytes 20 -ov -layout NONE -fs 'HFS+' -volname $VOL
+MOUNT=$(hdiutil attach $DMG)
+DISK=$(echo $MOUNT | sed -ne ' s|^/dev/\([^ ]*\).*$|\1|p')
+MOUNTPOINT=$(echo $MOUNT | sed -ne 's|^.*\(/Volumes/.*\)$|\1|p')
+ditto -rsrc "$EXPORT" "$MOUNTPOINT"
+chmod -R a+rX,u+w "$MOUNTPOINT"
+hdiutil detach $DISK
+hdiutil resize -sectors min $DMG
+ZDMG="${DMG:r}z.dmg"
+hdiutil convert $DMG -format UDBZ -o $ZDMG
+mv $ZDMG $DMG
+hdiutil internet-enable $DMG
+
+# sign the disk image
+codesign --sign $IDENTITY $DMG
+
+# verify disk image signature
+spctl -vv --assess --type open --context context:primary-signature $DMG
+
+# update appcast
+$PACKAGEDIR/Sparkle/bin/generate_appcast $PACKAGEDIR/dsa_priv.pem $RELEASEDIR
+
+# clean up
+rm -rf "$EXPORTDIR"
